c9e072546f575b37735713e4cf62e194c7035bdb
olivia barnett <livbarnett11@gmail.com>
2023-01-28 16:31:45 +0000

    rails watch list route one done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'destroying database'
+Movie.destroy_all
+puts 'creating database'
+json_films = JSON.parse(URI.open('http://tmdb.lewagon.com/movie/top_rated').read)
+json_films['results'].each do |film|
+  Movie.create(
+    title: film['title'],
+    overview: film['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500#{film['backdrop_path']}",
+    rating: film['vote_average']
+  )
+  puts 'seeded'
+end
+
+List.create!(name: "countrynwestern")
